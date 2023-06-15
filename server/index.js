b13d67de42e13dc60f74522f6cfcf7cb3eaef558
VuHoangDrinkCoffee <info.vuhoanglee@gmail.com>
2023-06-15 09:03:36 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -10,9 +10,16 @@ app.set('views', './views')
 server.listen(3000);
 
 io.on('connection', function(socket){
+
     console.log('new user connected: '+ socket.id)
+
     socket.on('disconnect', function(){
         console.log(socket.id+ " disconected")
+    })
+
+    socket.on('Client-send-data', (data) =>{
+        console.log(socket.id + ' sending ' + data)
+        io.sockets.emit('Server-send-data', data + ' sent')
     })
 });
 
