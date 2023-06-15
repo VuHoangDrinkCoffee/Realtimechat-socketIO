0e53d17868f552912cacc1dab68cc1a86a8b0010
VuHoangDrinkCoffee <info.vuhoanglee@gmail.com>
2023-06-15 12:11:38 +0700

    move into real

--- server/index.js
+++ server/index.js
@@ -1,27 +1,11 @@
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
-const io = require('socket.io')(server)
 
-app.use(express.static("/public"));
 app.set('view engine', 'ejs')
-app.set('views', './views')
-
+app.use(express.static('public'));
+app.use('/app', express.static('app'));
 server.listen(3000);
-
-io.on('connection', function(socket){
-
-    console.log('new user connected: '+ socket.id)
-
-    socket.on('disconnect', function(){
-        console.log(socket.id+ " disconected")
-    })
-
-    socket.on('Client-send-data', (data) =>{
-        console.log(socket.id + ' sending ' + data)
-        io.sockets.emit('Server-send-data', data)
-    })
-});
 
 app.get("/", function(req, res){
     res.render('trangchu');
