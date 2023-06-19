8f3ce38762951bb42e87f01c51c5245e0f651c27
VuHoangDrinkCoffee <info.vuhoanglee@gmail.com>
2023-06-19 16:33:20 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
+const { Socket } = require('dgram');
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
-app.use('/app', express.static('app'));
+app.use('/public', express.static('public'));
 server.listen(3000);
 
+io.on("connection", (socket)=>{
+    console.log(socket.id + " connected");
+})
 app.get("/", function(req, res){
     res.render('trangchu');
 })
