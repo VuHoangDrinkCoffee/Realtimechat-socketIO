470be45f70db5bb0faecb040c023f72357bbd019
VuHoangDrinkCoffee <info.vuhoanglee@gmail.com>
2023-06-20 14:21:53 +0700

    .

--- server/index.js
+++ server/index.js
@@ -1,17 +1,31 @@
-const { Socket } = require('dgram');
-var express = require('express')
+const { Socket } = require("dgram");
+var express = require("express");
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
-app.use('/public', express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use("/public", express.static("public"));
 server.listen(3000);
 
-io.on("connection", (socket)=>{
-    console.log(socket.id + " connected");
-})
-app.get("/", function(req, res){
-    res.render('trangchu');
-})
+var userOnline = [];
+
+io.on("connection", (socket) => {
+  console.log(socket.id + " connected");
+  socket.on("client-send-userName", (data) => {
+    if (userOnline.includes(data) >= 1) {
+      socket.emit("user-send-failed");
+    } else {
+        userOnline.push(data);
+        socket.Username = data;
+        socket.emit('user-send-succes', data)
+        console.log(userOnline);
+        io.sockets.emit('sever-send-onlineUser', userOnline)
+    }
+
+  });
+});
+app.get("/", function (req, res) {
+  res.render("trangchu");
+});
--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -1,5 +1,24 @@
-const socket = io('http://localhost:3000/');
-$(document).ready(function(){
-$('.loginForm').show();
-$('.chatForm').hide();
+const socket = io("http://localhost:3000/");
+
+socket.on("user-send-failed", () => {
+  alert("WRONG");
+});
+socket.on("user-send-succes", (data) => {
+  $("#currenUser").html(data);
+  $(".chatForm").show(1000);
+  $(".loginForm").hide(1500);
+});
+socket.on('sever-send-onlineUser', (data)=>{
+    $('#boxContent').html('');
+    data.forEach((i)=>{
+        $('#boxContent').append("<div class='user'>" + i + "<div>")
+    }); 
+})
+$(document).ready(function () {
+  $(".loginForm").show();
+  $(".chatForm").hide();
+
+  $("#btnRegister").click(() => {
+    socket.emit("client-send-userName", $("#txtUsername").val());
+  });
 });
