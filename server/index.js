6d144a58893d0319910aff89e275ab196bd0b139
VuHoangDrinkCoffee <info.vuhoanglee@gmail.com>
2023-06-21 08:57:13 +0700

    complete basci

--- server/index.js
+++ server/index.js
@@ -17,14 +17,26 @@ io.on("connection", (socket) => {
     if (userOnline.includes(data) >= 1) {
       socket.emit("user-send-failed");
     } else {
-        userOnline.push(data);
-        socket.Username = data;
-        socket.emit('user-send-succes', data)
-        console.log(userOnline);
-        io.sockets.emit('sever-send-onlineUser', userOnline)
+      userOnline.push(data);
+      socket.Username = data;
+      socket.emit("user-send-succes", data);
+      io.sockets.emit("sever-send-onlineUser", userOnline);
     }
-
+    socket.on("logout", () => {
+      userOnline.splice(userOnline.indexOf(socket.Username), 1);
+      socket.broadcast.emit("sever-send-onlineUser", userOnline);
+    });
   });
+  socket.on("user-send-messages", (data) => {
+    io.sockets.emit("server-send-messages", { un: socket.Username, nd: data });
+  });
+  socket.on('typing-by-user', () =>{
+    var s = 'Typing by ' + socket.Username;
+    socket.broadcast.emit('typing-by-someones', s)
+  })
+  socket.on('stop-typing-by-user', () =>{
+    socket.broadcast.emit('stop-typing-by-someones')
+  })
 });
 app.get("/", function (req, res) {
   res.render("trangchu");
--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -8,17 +8,43 @@ socket.on("user-send-succes", (data) => {
   $(".chatForm").show(1000);
   $(".loginForm").hide(1500);
 });
-socket.on('sever-send-onlineUser', (data)=>{
-    $('#boxContent').html('');
-    data.forEach((i)=>{
-        $('#boxContent').append("<div class='user'>" + i + "<div>")
-    }); 
+socket.on("sever-send-onlineUser", (data) => {
+  $("#boxContent").html("");
+  data.forEach((i) => {
+    $("#boxContent").append("<div class='user'>" + i + "</div>");
+  });
+});
+socket.on("server-send-messages", (data) => {
+  $("#listMessages").append(
+    "<div class='ms'>" + data.un + ": " + data.nd + "</div>"
+  );
+});
+socket.on('typing-by-someones', (data) =>{
+  $("#noti").html(data);
+})
+socket.on('stop-typing-by-someones', (data) =>{
+  $("#noti").html("");
 })
 $(document).ready(function () {
   $(".loginForm").show();
   $(".chatForm").hide();
+  $("#txtMessages").focusin(() =>{
+    socket.emit('typing-by-user');
+  });
+  $("#txtMessages").focusout(() =>{
+    socket.emit('stop-typing-by-user');
+  })
 
   $("#btnRegister").click(() => {
     socket.emit("client-send-userName", $("#txtUsername").val());
+  });
+  $("#bthLogout").click(() => {
+    socket.emit("logout");
+    $(".chatForm").hide(2000);
+    $(".loginForm").show(1000);
+  });
+  $("#btnSubmit").click(() => {
+    socket.emit("user-send-messages", $("#txtMessages").val());
+    $("#txtUsername").val("");
   });
 });
